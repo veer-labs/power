2b036875126e4ee54dc306efa927944769e8d498
Eric Butter <hola@ericbeurre.com>
2012-11-20 23:04:52 -0500

    little simulation of expected parasites found per FOV; TODO rgl 3d graph ugly; TODO sample size required to diagnose at each FOV/parasitemia

---
+++ powersim.R
@@ -0,0 +1,10 @@
+rbc_per_ul <- 5e6
+parasites_per_ul_pyrogenic_nonimmune <- 1e4
+parasites_per_ul_pyrogenic_immune <- 1e5
+parasitemia_clinical_nonimmune <- parasites_per_ul_pyrogenic_nonimmune/rbc_per_ul
+parasitemia_clinical_immune <- parasites_per_ul_pyrogenic_immune/rbc_per_ul
+slide_length_mm <- 75
+slide_width_mm <- 25
+sample_volume_ul <- 30
+smear_depth_mm <- 30/(75*25*2/3)
+
